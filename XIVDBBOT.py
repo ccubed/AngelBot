1ffbc9d63fa0dd365977541c5ccd8388a15c4580
Cooper <ccubed.techno@gmail.com>
2016-04-02 19:34:31 -0400

    Added class outline for XIVDBBOT

--- XIVDBBOT.py
+++ XIVDBBOT.py
@@ -1,2 +1,60 @@
 from urllib.request import urlopen
 
+
+class DBParser:
+    def __init__(self):
+        pass
+
+    def searchall(self, name):
+        pass
+
+    def searchone(self, name, itype):
+        pass
+
+    def parseitem(self, name):
+        pass
+
+    def parsequest(self, name):
+        pass
+
+    def parserecipe(self, name):
+        pass
+
+    def parseinstance(self, name):
+        pass
+
+    def parseaction(self, name):
+        pass
+
+    def parseplace(self, name):
+        pass
+
+    def parseshop(self, name):
+        pass
+
+    def parsegather(self, name):
+        pass
+
+    def parsenpc(self, name):
+        pass
+
+    def parseenemy(self, name):
+        pass
+
+    def parseemote(self, name):
+        pass
+
+    def parsestatus(self, name):
+        pass
+
+    def parsetitle(self, name):
+        pass
+
+    def parseminion(self, name):
+        pass
+
+    def parsemount(self, name):
+        pass
+
+    def parsehdim(self, name):
+        pass
