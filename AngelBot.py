c1ced761784c74d3e1a5e704e989b7e0c5d583c8
Charles Click <ccubed.techno@gmail.com>
2016-04-03 02:41:54 -0400

    Alpha 1 of the AngelBot

--- AngelBot.py
+++ AngelBot.py
@@ -1,10 +1,28 @@
 import discord
 import random
-import logging
 from discord.ext import commands
-from XIVLS import *
 from XIVDBBOT import *
 
 
+class AngelBot(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.XIVDB = DBParser()
+
+    @client.async_event
+    def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$help'):
+            await self.send_message(message.author,
+                              "Help for AngelBot\n\n- XIVDB Commands -\n!search <name or ID> - Search for something on XIVDB\n!item <name or ID> - Search for an item. Items are weapons, armor, etc.\n!quest <name or ID> - Search for a quest.\n!recipe <name or ID> - Search for a crafting recipe.\n!action <name or ID> - Search for a skill.\n!mats <name or ID> - Search for the location details on a gatherable material.\n!npc <name or ID> - Search for details on an NPC.\n!effect <name or ID> - Search for information on a status effect.\n!minion <name or ID> - Search for information on a minion.\n!achievement <name or ID> - Search for information on an achievement.\n!hdim <name or ID> - How do I make...Search for instructions on making a thing.\n!wdif <name> - Where do I find...Search for the locations of items.")
 
 
+    @client.async_event
+    def on_ready(self):
+        print('Logged in')
+
+
+bot = AngelBot()
+bot.run('email', 'password')
--- XIVDBBOT.py
+++ XIVDBBOT.py
@@ -5,14 +5,18 @@ import json
 
 class DBParser:
     def __init__(self):
-        self.apiurl = "http://api.xivdb.com"
-        self.shortnames = {'ARC':'Archer', 'GLA':'Gladiator', 'LNC':'Lancer', 'MRD':'Marauder', 'PGL':'Pugilist', 'ACN':'Arcanist', 'CNJ':'Conjurer', 'THM':'Thaumaturge', 'ROG':'Rogue',
-                           'BRD':'Bard', 'DRG':'Dragoon', 'MNK':'Monk', 'PLD':'Paladin', 'WAR':'Warrior', 'BLM':'Black Mage', 'WHM':'White Mage', 'SCH':'Scholar', 'SMN':'Summoner',
-                           'NIN':'Ninja', 'AST':'Astrologian', 'DRK':'Dark Knight', 'MCH':'Machinist', 'ALC':'Alchemist', 'ARM':'Armorer', 'BSM':'Blacksmith', 'CRP':'Carpenter',
-                           'CUL':'Culinerian', 'GSM':'Goldsmith', 'LTW':'Leatherworker', 'WVR':'Weaver', 'BTN':'Botanist', 'FSH':'Fisher', 'MIN':'Miner'}
+        self.apiurl = "https://api.xivdb.com"
+        self.shortnames = {'ARC': 'Archer', 'GLA': 'Gladiator', 'LNC': 'Lancer', 'MRD': 'Marauder', 'PGL': 'Pugilist',
+                           'ACN': 'Arcanist', 'CNJ': 'Conjurer', 'THM': 'Thaumaturge', 'ROG': 'Rogue',
+                           'BRD': 'Bard', 'DRG': 'Dragoon', 'MNK': 'Monk', 'PLD': 'Paladin', 'WAR': 'Warrior',
+                           'BLM': 'Black Mage', 'WHM': 'White Mage', 'SCH': 'Scholar', 'SMN': 'Summoner',
+                           'NIN': 'Ninja', 'AST': 'Astrologian', 'DRK': 'Dark Knight', 'MCH': 'Machinist',
+                           'ALC': 'Alchemist', 'ARM': 'Armorer', 'BSM': 'Blacksmith', 'CRP': 'Carpenter',
+                           'CUL': 'Culinerian', 'GSM': 'Goldsmith', 'LTW': 'Leatherworker', 'WVR': 'Weaver',
+                           'BTN': 'Botanist', 'FSH': 'Fisher', 'MIN': 'Miner'}
 
     def searchall(self, name):
-        data = {'string': name}
+        data = {'string': name.replace(' ', '+')}
         data = urllib.parse.urlencode(data)
         url = self.apiurl + '/search?' + data
         response = urllib.request.urlopen(url)
@@ -29,14 +33,14 @@ class DBParser:
         key = itype.lower() if itype.endswith('s') else itype.lower() + 's'
         if key is 'gatherings':
             key = 'gathering'
-        data = {'string': name, 'one': key }
+        data = {'string': name, 'one': key}
         data = urllib.parse.urlencode(data)
         url = self.apiurl + '/search?' + data
         response = urllib.request.urlopen(url)
         jd = response.read().decode('utf-8')
         jd = json.loads(jd)
         if jd[key]['total'] == 0:
-            return "No results for {0} within {1}.".format(name,key)
+            return "No results for {0} within {1}.".format(name, key)
         elif jd[key]['total'] == 1:
             if key is 'items':
                 return self.parseitem(jd[key]['results'][0]['id'])
@@ -73,17 +77,29 @@ class DBParser:
         elif jd[key]['total'] <= 5:
             message = "Matched more than one result. Try searching by ID.\n"
             for items in jd[key]['results']:
-                message += "{0} (ID: {1})\n".format(items['name'],items['id'])
+                message += "{0} (ID: {1})\n".format(items['name'], items['id'])
             return message
         else:
             return "Returned {0} results. Add more words to search.".format(jd[key]['total'])
+
+    def searchid(self, name):
+        data = {'string': name.replace(' ', '+')}
+        data = urllib.parse.urlencode(data)
+        url = self.apiurl + '/search?' + data
+        response = urllib.request.urlopen(url)
+        jd = response.read().decode('utf-8')
+        jd = json.loads(jd)
+        return jd
 
     def parseitem(self, name):
         url = self.apiurl + '/item/' + name
         response = urllib.request.urlopen(url)
         jd = response.read().decode('utf-8')
         jd = json.loads(jd)
-        message = "{0}\n{1} {2} {3} {4}\n{5}".format(jd['name'], 'Unique' if jd['is_unique'] else 'Common', 'Tradeable' if jd['is_tradable'] else 'Untradable', 'Desynth' if jd['is_desynthesizable'] or 'No_Desynth', 'Dyeable' if jd['is_dyeable'] or 'Undyeable', jd['url_xivdb'])
+        message = "{0}\n{1} {2} {3} {4}\n{5}".format(jd['name'], 'Unique' if jd['is_unique'] else 'Common',
+                                                     'Tradeable' if jd['is_tradable'] else 'Untradable',
+                                                     'Desynth' if jd['is_desynthesizable'] else 'No_Desynth',
+                                                     'Dyeable' if jd['is_dyeable'] else 'Undyeable', jd['url_xivdb'])
         return message
 
     def parsequest(self, name):
@@ -94,57 +110,133 @@ class DBParser:
         message = "{0}\n".format(jd['name'])
         if jd['classjob_category_1']['name'] in self.shortnames:
             message += "Related Class: {0}\n".format(shortnames[jd['classjob_category_1']['name']])
-        message += "Start this quest with {0} in {1} at {2}x {3}y\n".format(jd['npc_start']['name'], jd['npc_start']['placename']['name'], jd['npc_start']['position']['x'], jd['npc_start']['position']['y'])
+        message += "Start this quest with {0} in {1} at {2}x {3}y\n".format(jd['npc_start']['name'],
+                                                                            jd['npc_start']['placename']['name'],
+                                                                            jd['npc_start']['position']['x'],
+                                                                            jd['npc_start']['position']['y'])
         message += jd['url_xivdb']
         return message
 
     def parserecipe(self, name):
-        url = self.apiurl + "/quest/" + name
+        url = self.apiurl + "/recipe/" + name
         response = urllib.request.urlopen(url)
         jd = response.read().decode('utf-8')
         jd = json.loads(jd)
+        message = "{0}\nClass: {1} Recipe Level: {2}\n".format(jd['name'], jd['classjob']['name'], jd['level'])
+        message += "Materials Required:\n"
+        for item in jd['_tree']:
+            message += "   {0} {1} (ID:{2})".format(item['quantity'], item['name'], item['id'])
+        message += jd['url_xivdb']
+        return message
 
     def parseinstance(self, name):
-        pass
+        return "I don't parse dungeons. There isn't really any useful information on XIVDB about dungeons."
 
     def parseaction(self, name):
-        pass
+        url = self.apiurl + "/recipe/" + name
+        response = urllib.request.urlopen(url)
+        jd = response.read().decode('utf-8')
+        jd = json.loads(jd)
+        return "{0}\nClass: {1} Level: {2}\n{3}\n{4}".format(jd['name'], jd['classjob']['name'], jd['level'],
+                                                             jd['help'], jd['url_xivdb'])
 
     def parseplace(self, name):
-        pass
+        return "I don't parse places. There isn't really any useful information on XIVDB about places."
 
     def parseshop(self, name):
-        pass
+        return "This doesn't seem to be in the api currently."
 
     def parsegather(self, name):
-        pass
+        url = self.apiurl + "/gathering/" + name
+        response = urllib.request.urlopen(url)
+        jd = response.read().decode('utf-8')
+        jd = json.loads(jd)
+        message = "{0}\nGathered from:\n".format(jd['name'])
+        for node in jd['nodes']:
+            message += "   {0} - {1} - {2}\n".format(node['region']['name'], node['zone']['name'],
+                                                     node['placename']['name'])
+        return message
 
     def parsenpc(self, name):
-        pass
+        url = self.apiurl + "/npc/" + name
+        response = urllib.request.urlopen(url)
+        jd = response.read().decode('utf-8')
+        jd = json.loads(jd)
+        message = "{0} is located in {1} at {2}x {3}y".format(jd['name'], jd['placename']['name'],
+                                                              jd['coordinates']['x'], jd['coordinates']['y'])
+        return message
 
     def parseenemy(self, name):
-        pass
+        return "Eveb if an enemy is parsed, since the change to XIVDB version 2 all the position data on enemies went away."
 
     def parseemote(self, name):
-        pass
+        return "I don't parse emotes. There isn't really any useful information on XIVDB about emotes."
 
     def parsestatus(self, name):
-        pass
+        url = self.apiurl + "/status/" + name
+        response = urllib.request.urlopen(url)
+        jd = response.read().decode('utf-8')
+        jd = json.loads(jd)
+        return "Name: {0}\n{1}".format(jd['name'], jd['help'])
 
     def parsetitle(self, name):
-        pass
+        return "I don't parse titles. There isn't really any useful information on XIVDB about titles."
 
     def parseminion(self, name):
-        pass
+        url = self.apiurl + "/status/" + name
+        response = urllib.request.urlopen(url)
+        jd = response.read().decode('utf-8')
+        jd = json.loads(jd)
+        message = "{0}\n   Minion Action: {1}\n   {2}".format(jd['name'], jd['action'], jd['help'])
+        return message
 
     def parsemount(self, name):
-        pass
-
-    def parsehdim(self, name):
-        pass
+        return "I don't parse mounts. There isn't really any useful information on XIVDB about mounts."
 
     def parseachievement(self, name):
-        pass
+        url = self.apiurl + "/achievement/" + name
+        response = urllib.request.urlopen(url)
+        jd = response.read().decode('utf-8')
+        jd = json.loads(jd)
+        return "{0} ({1} - {2})\n{3}".format(jd['name'], jd['category_name'], jd['kind_name'], jd['help'])
+
+    def parsehdim(self, name):
+        if name.isnumeric():
+            return self.parserecipe(name)
+        else:
+            jd = self.searchid(name)
+            if jd['recipes']['total'] > 1:
+                return "{0} matched more than 1 recipe. Maybe try finding the ID first with search?".format(name)
+            elif jd['recipes']['total'] == 0:
+                return "{0} didn't match a recipe.".format(name)
+            else:
+                return self.parserecipe(jd['recipes']['results'][0]['id'])
 
     def parsewdif(self, name):
-        pass
+        jd = self.searchid(name)
+        if jd['recipes']['total'] > 1:
+            message = "{0} matched multiple items. Telling you how to make them.".format(name)
+            for item in jd['recipes']['results']:
+                url = self.apiurl + "/recipe/" + item['id']
+                response = urllib.request.urlopen(url)
+                jd = response.read().decode('utf-8')
+                jd = json.loads(jd)
+                message += "{0} ({1} LV.{2})\n".format(jd['name'], jd['classjob']['abbr'], jd['level'])
+                list = []
+                for item in jd['_tree']:
+                    list.append("{0} {1}".format(item['quantity'], item['name']))
+                message += ','.join(list)
+                message += "\n"
+            return message
+        else:
+            url = self.apiurl + "/recipe/" + jd['recipes']['results'][0]
+            response = urllib.request.urlopen(url)
+            jd = response.read().decode('utf-8')
+            jd = json.loads(jd)
+            message = "{0} ({1} LV.{2})\n".format(jd['name'], jd['classjob']['abbr'], jd['level'])
+            list = []
+            for item in jd['_tree']:
+                list.append("{0} {1}".format(item['quantity'], item['name']))
+            message += ','.join(list)
+            message += "\n"
+            return message
