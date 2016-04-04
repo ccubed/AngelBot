8db23bd0de021a9808c968f1d5e83fe61f0eb80c
Cooper <ccubed.techno@gmail.com>
2016-04-04 14:14:03 -0400

    Globals

---
+++ globals.py
@@ -0,0 +1,20 @@
+dungeons = {'Sastasha Seagrot': {}, 'Tam-Tara Deepcroft': {}, 'Copperbell Mines': {}, 'Thousand Maws of Toto-Rak': {}}
+roulettes = ['Leveling', 'Level 50', 'Level 60', 'Expert']
+raids = {'Binding Coil of Bahamut': {'Stages': 5, 'Extreme': 0, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}, 'full': 0},
+         'Second Coil of Bahamut': {'Stages': 4, 'Extreme': 1, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}, 'full': 0},
+         'Final Coil of Bahamut': {'Stages': 4, 'Extreme': 0, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}, 'full': 0},
+         'Alexander Gordias': {'Stages': 4, 'Extreme': 1, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}, 'full': 0},
+         'Alexander Midas': {'Stages': 4, 'Extreme': 1, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}, 'full': 0},
+         'Void Ark': {'Stages': 1, 'Extreme': 0, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}, 'full': 1},
+         'Labyrinth of the Ancients': {'Stages': 1, 'Extreme': 0, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}, 'full': 1},
+         'Syrcus Tower': {'Stages': 1, 'Extreme': 0, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}, 'full': 1},
+         'World of Darkness': {'Stages': 1, 'Extreme': 0, 'party': {'Tank': 2, 'Healers': 2, 'DPS': 4}}, 'full': 1}
+trials = {'Ifrit': ['Normal', 'Hard', 'Extreme'], 'Titan': ['Normal', 'Hard', 'Extreme'],
+          'Garuda': ['Normal', 'Hard', 'Extreme'], 'Rihtahtyn sas Arvina': ['Normal'], 'Gilgamesh': ['Normal'],
+          'Chimera': ['Normal'], 'Hydra': ['Normal'],
+          'Good King Moogle Mog XII': ['Hard', 'Extreme'], 'Ultima Weapon': ['Normal'],
+          'Leviathan': ['Hard', 'Extreme'], 'Ramuh': ['Hard', 'Extreme'], "The Dragon's Neck": ['Normal'],
+          'Shiva': ['Hard', 'Extreme'], 'Nabriales': ['Normal'],
+          'Vishap': ['Normal'], 'Odin': ['Normal'], 'Ravana': ['Hard', 'Extreme'],
+          'King Thordan': ['Normal', 'Extreme'], 'Bismarck': ['Hard', 'Extreme'], 'Sephirot': ['Normal', 'Extreme']}
+parties = {'light': {'Tank': 1, 'Healer': 1, 'DPS': 2}, 'full': {'Tank': 2, 'Healer': 2, 'DPS': 4}}
