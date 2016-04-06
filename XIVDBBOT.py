78c66fdd9fb83e24c226024071f4607c0cdf96c6
Cooper <ccubed.techno@gmail.com>
2016-04-06 18:31:56 -0400

    Attempting a beta version of a module based command stack.

--- XIVDBBOT.py
+++ XIVDBBOT.py
@@ -14,6 +14,7 @@ class DBParser:
                            'ALC': 'Alchemist', 'ARM': 'Armorer', 'BSM': 'Blacksmith', 'CRP': 'Carpenter',
                            'CUL': 'Culinerian', 'GSM': 'Goldsmith', 'LTW': 'Leatherworker', 'WVR': 'Weaver',
                            'BTN': 'Botanist', 'FSH': 'Fisher', 'MIN': 'Miner'}
+        self.commands = [['$search', self.searchall, 0], ['$item', self.searchitem, 0], ['$quest', self.searchquest, 0], ['$recipe', self.searchrecipe, 0], ['$action', self.searchaction, 0], ['$mats', self.searchmats, 0], ['$npc', self.searchnpc, 0], ['$effect', self.searchstatus, 0], ['$minion', self.searchminion, 0], ['$achievement', self.searchachievement, 0], ['$hdim', self.parsehdim, 0], ['$wdif', self.parsewdif, 1]]
 
     def searchall(self, name):
         data = {'string': name}
@@ -29,59 +30,6 @@ class DBParser:
         message += "Ask for a specific category or add more words."
         return message
 
-    def searchone(self, name, itype):
-        key = itype.lower() if itype.endswith('s') else itype.lower() + 's'
-        if key == 'gatherings':
-            key = 'gathering'
-        data = {'string': name, 'one': key}
-        data = urllib.parse.urlencode(data)
-        url = self.apiurl + '/search?' + data
-        response = urllib.request.urlopen(url)
-        jd = response.read().decode('utf-8')
-        jd = json.loads(jd)
-        if jd[key]['total'] == 0:
-            return "No results for {0} within {1}.".format(name, key)
-        elif jd[key]['total'] == 1:
-            if key == 'items':
-                return self.parseitem(str(jd[key]['results'][0]['id']))
-            elif key == 'quests':
-                return self.parsequest(str(jd[key]['results'][0]['id']))
-            elif key == 'achievements':
-                return self.parseachievement(str(jd[key]['results'][0]['id']))
-            elif key == 'recipes':
-                return self.parserecipe(str(jd[key]['results'][0]['id']))
-            elif key == 'instances':
-                return self.parseinstance(str(jd[key]['results'][0]['id']))
-            elif key == 'actions':
-                return self.parseaction(str(jd[key]['results'][0]['id']))
-            elif key == 'places':
-                return self.parseplace(str(jd[key]['results'][0]['id']))
-            elif key == 'shops':
-                return self.parseshop(str(jd[key]['results'][0]['id']))
-            elif key == 'gathering':
-                return self.parsegather(str(jd[key]['results'][0]['id']))
-            elif key == 'npcs':
-                return self.parsenpc(str(jd[key]['results'][0]['id']))
-            elif key == 'enemies':
-                return self.parseenemy(str(jd[key]['results'][0]['id']))
-            elif key == 'emotes':
-                return self.parseemote(str(jd[key]['results'][0]['id']))
-            elif key == 'status':
-                return self.parsestatus(str(jd[key]['results'][0]['id']))
-            elif key == 'titles':
-                return self.parsetitle(str(jd[key]['results'][0]['id']))
-            elif key == 'minions':
-                return self.parseminion(str(jd[key]['results'][0]['id']))
-            elif key == 'mounts':
-                return self.parsemount(str(jd[key]['results'][0]['id']))
-        elif jd[key]['total'] <= 5:
-            message = "Matched more than one result. Try searching by ID.\n"
-            for items in jd[key]['results']:
-                message += "{0} (ID: {1})\n".format(items['name'], items['id'])
-            return message
-        else:
-            return "Returned {0} results. Add more words to search.".format(jd[key]['total'])
-
     def searchid(self, name):
         data = {'string': name}
         data = urllib.parse.urlencode(data)
@@ -90,6 +38,159 @@ class DBParser:
         jd = response.read().decode('utf-8')
         jd = json.loads(jd)
         return jd
+
+    def searchitem(self, name):
+        data = {'string': name, 'one': 'items'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['items']['total'] == 0:
+            return "No results for {0} in items.".format(name)
+        elif jd['items']['total'] == 1:
+            return self.parseitem(str(jd['items']['results'][0]['id']))
+        elif jd['items']['total'] <= 5:
+            message = "Matched more than one item. Try searching by ID.\n"
+            for items in jd['items']['results']:
+                message += "{0} (ID: {1})\n".format(items['name'], items['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['items']['total'])
+
+    def searchquest(self, name):
+        data = {'string': name, 'one': 'quests'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['quests']['total'] == 0:
+            return "No results for {0} in quests.".format(name)
+        elif jd['quests']['total'] == 1:
+            return self.parsequest(str(jd['quests']['results'][0]['id']))
+        elif jd['quests']['total'] <= 5:
+            message = "Matched more than one quest. Try searching by ID.\n"
+            for quests in jd['quests']['results']:
+                message += "{0} (ID: {1})\n".format(quests['name'], quests['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['quests']['total'])
+
+    def searchrecipe(self, name):
+        data = {'string': name, 'one': 'recipes'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['recipes']['total'] == 0:
+            return "No results for {0} in recipes.".format(name)
+        elif jd['recipes']['total'] == 1:
+            return self.parserecipe(str(jd['recipes']['results'][0]['id']))
+        elif jd['recipes']['total'] <= 5:
+            message = "Matched more than one recipe. Try searching by ID.\n"
+            for recipes in jd['recipes']['results']:
+                message += "{0} (ID: {1})\n".format(recipes['name'], recipes['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['recipes']['total'])
+
+    def searchaction(self, name):
+        data = {'string': name, 'one': 'actions'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['actions']['total'] == 0:
+            return "No results for {0} in actions.".format(name)
+        elif jd['actions']['total'] == 1:
+            return self.parseaction(str(jd['actions']['results'][0]['id']))
+        elif jd['actions']['total'] <= 5:
+            message = "Matched more than one action. Try searching by ID.\n"
+            for actions in jd['actions']['results']:
+                message += "{0} (ID: {1})\n".format(actions['name'], actions['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['actions']['total'])
+
+    def searchmats(self, name):
+        data = {'string': name, 'one': 'gathering'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['gathering']['total'] == 0:
+            return "No results for {0} in gathering.".format(name)
+        elif jd['gathering']['total'] == 1:
+            return self.parsegather(str(jd['gathering']['results'][0]['id']))
+        elif jd['gathering']['total'] <= 5:
+            message = "Matched more than one material. Try searching by ID.\n"
+            for gathering in jd['gathering']['results']:
+                message += "{0} (ID: {1})\n".format(gathering['name'], gathering['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['gathering']['total'])
+
+    def searchnpc(self, name):
+        data = {'string': name, 'one': 'npcs'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['npcs']['total'] == 0:
+            return "No results for {0} in npcs.".format(name)
+        elif jd['npcs']['total'] == 1:
+            return self.parsenpc(str(jd['npcs']['results'][0]['id']))
+        elif jd['npcs']['total'] <= 5:
+            message = "Matched more than one npc. Try searching by ID.\n"
+            for npcs in jd['npcs']['results']:
+                message += "{0} (ID: {1})\n".format(npcs['name'], npcs['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['npcs']['total'])
+
+    def searcheffect(self, name):
+        data = {'string': name, 'one': 'status'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['status']['total'] == 0:
+            return "No results for {0} in status.".format(name)
+        elif jd['status']['total'] == 1:
+            return self.parsestatus(str(jd['status']['results'][0]['id']))
+        elif jd['status']['total'] <= 5:
+            message = "Matched more than one status effect. Try searching by ID.\n"
+            for status in jd['status']['results']:
+                message += "{0} (ID: {1})\n".format(status['name'], status['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['status']['total'])
+
+    def searchminion(self, name):
+        data = {'string': name, 'one': 'minions'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['minions']['total'] == 0:
+            return "No results for {0} in minions.".format(name)
+        elif jd['minions']['total'] == 1:
+            return self.parseminion(str(jd['minions']['results'][0]['id']))
+        elif jd['minions']['total'] <= 5:
+            message = "Matched more than one minion. Try searching by ID.\n"
+            for minions in jd['minions']['results']:
+                message += "{0} (ID: {1})\n".format(minions['name'], minions['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['minion']['total'])
+
+    def searchachievement(self, name):
+        data = {'string': name, 'one': 'achievements'}
+        url = self.apiurl + '/search?' + urllib.parse.urlencode(data)
+        response = urllib.request.urlopen(url)
+        jd = json.loads(response.read().decode('utf-8'))
+        if jd['achievements']['total'] == 0:
+            return "No results for {0} in achievements.".format(name)
+        elif jd['achievements']['total'] == 1:
+            return self.parserecipe(str(jd['achievements']['results'][0]['id']))
+        elif jd['achievements']['total'] <= 5:
+            message = "Matched more than one achievement. Try searching by ID.\n"
+            for achievements in jd['achievements']['results']:
+                message += "{0} (ID: {1})\n".format(achievements['name'], achievements['id'])
+            return message
+        else:
+            return "Returned {0} results. Add more words to search.".format(jd['achievements']['total'])
 
     def parseitem(self, name):
         url = self.apiurl + '/item/' + name
@@ -112,8 +213,10 @@ class DBParser:
             message += "Related Class: {0}\n".format(self.shortnames[jd['classjob_category_1']['name']])
         message += "Start this quest with {0} in {1} at {2}x {3}y\n".format(jd['npc_start']['name'],
                                                                             jd['npc_start']['placename']['name'],
-                                                                            jd['npc_start']['position']['x'] if jd['npc_start']['position'] != '' else '0',
-                                                                            jd['npc_start']['position']['y'] if jd['npc_start']['position'] != '' else '0')
+                                                                            jd['npc_start']['position']['x'] if
+                                                                            jd['npc_start']['position'] != '' else '0',
+                                                                            jd['npc_start']['position']['y'] if
+                                                                            jd['npc_start']['position'] != '' else '0')
         if 'pre_quest' in jd:
             message += "Prerequisite Quests ->\n"
             for item in jd['pre_quest']:
@@ -154,7 +257,8 @@ class DBParser:
         jd = json.loads(jd)
         message = "{0} (ID: {1})\nThis shops sells ->\n".format(jd['npc_name'], jd['id'])
         for item in jd['items']:
-            message += "   {0} (ID: {1}) for {2}gil\n".format(item['item']['name'], item['item']['id'], item['item']['price_mid'])
+            message += "   {0} (ID: {1}) for {2}gil\n".format(item['item']['name'], item['item']['id'],
+                                                              item['item']['price_mid'])
         return message
 
     def parsegather(self, name):
@@ -282,7 +386,8 @@ class DBParser:
             if 'gathering' in jd:
                 if jd['gathering']['total']:
                     for item in jd['gathering']['results']:
-                        gathering.append('{0} Lv.{1} (Node ID: {2})'.format(item['type_name'], item['level'], item['id']))
+                        gathering.append(
+                            '{0} Lv.{1} (Node ID: {2})'.format(item['type_name'], item['level'], item['id']))
             message = []
             if len(achievements):
                 message.append("Obtained from Achievements ->\n")
