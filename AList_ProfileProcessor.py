2d9fb22cb9cfd2cc439b0fdf60369586f66f98c5
Cooper <ccubed.techno@gmail.com>
2016-05-18 23:23:17 -0400

    Make Profile Preprocessor a little faster. It doesn't take 14 seconds now!

--- AList_ProfileProcessor.py
+++ AList_ProfileProcessor.py
@@ -1,53 +1,47 @@
 import re
 
+
 def profile_preprocessor(content):
     # Compile Regular Expressions
-    imgtag = re.compile("img[0-9]{0,5}?\((?P<location>.*?)\)", re.IGNORECASE)
-    mdimg = re.compile("\[(?P<name>.*?)\]\((?P<location>.*?)\)", re.IGNORECASE)
-    atags = re.compile('<a href="(?P<location>.*?)">(?P<title>.*?)</a>', re.IGNORECASE)
+    imgtag = re.compile("img.*?\((?P<location>.*?)\)", re.IGNORECASE)
+    mdimg = re.compile("\[.*?\]\((?P<location>.*?)\)", re.IGNORECASE)
+    atags = re.compile('<a.*?>(?P<title>.*?)</a>', re.IGNORECASE)
     smalltags = re.compile('<small>', re.IGNORECASE)
-    fontcolors = re.compile('<font color="(?P<color>.*?)">(?P<content>.*?)</font>', re.IGNORECASE)
+    fontcolors = re.compile('<font.*?>(?P<content>.*?)</font>', re.IGNORECASE)
+    breaks = re.compile('<br.*?>', re.IGNORECASE)
+    blocks = re.compile('(~!|!~)', re.IGNORECASE)
+    removes = re.compile('(~|#|<p>|<hr>)', re.IGNORECASE)
     # Create a placeholder that we'll change
     final = content
     # Do inplace modifications for known tags with no information attached
-    final = final.replace('~', '')
-    final = final.replace('#', '')
-    final = final.replace('<p>', '')
-    final = final.replace('<hr>', '')
     final = final.replace('__', '***')
-    final = final.replace('<br />', '\n')
-    final = final.replace('<br>', '\n')
-    final = final.replace('<br/>', '\n')
-    final = final.replace('~!', '`')
-    final = final.replace('!~', '`')
+    final = breaks.sub('\n', final)
+    final = blocks.sub('\`', final)
+    final = removes.sub('', final)
     # Create a list for these next functions
     final = final.split("\n")
     # Create a list from the current lines in final and find any line that starts with - and replace with • (For lists)
-    iterable = final
-    for num, line in enumerate(iterable):
-        if line[0:1] == '-':
+    for num, line in enumerate(final):
+        temp = line
+        if temp[0:1] == '-':
             temp = list(line)
             temp[0] = '•'
-            final[num] = temp
-    # Set iterable equal to the new version of Final and run our regex's against it
-    iterable = final
-    for num, line in enumerate(iterable):
-        temp = line
-        imgs = imgtag.findall(line)
-        mds = mdimg.findall(line)
-        links = atags.findall(line)
-        smalls = smalltags.findall(line)
-        fonts = fontcolors.findall(line)
+            temp = ''.join(temp)
+        imgs = imgtag.findall(temp)
+        mds = mdimg.findall(temp)
+        links = atags.findall(temp)
+        smalls = smalltags.findall(temp)
+        fonts = fontcolors.findall(temp)
         # Check to see if any matched, then replace
         if imgs:
             for matches in imgs:
-                temp = temp.replace(matches[0], matches[1])
+                temp = temp.replace(matches[0], '<{0}>'.format(matches[1]))
         if mds:
             for matches in mds:
-                temp = temp.replace("[{0}]({1})".format(matches[0], matches[1]), matches[0] + ": " + matches[1])
+                temp = temp.replace("[{0}]({1})".format(matches[0], matches[1]), '<{0}>'.format(matches[1]))
         if links:
             for matches in links:
-                temp = temp.replace('<a href="{0}">{1}</a>'.format(matches[0], matches[1]), matches[0] + ": " + matches[1])
+                temp = temp.replace('<a href="{0}">{1}</a>'.format(matches[0], matches[1]), '<{0}>'.format(matches[1]))
         if smalls:
             for matches in smalls:
                 temp = temp.replace("<small>", "*")
