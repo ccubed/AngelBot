01cf9ce90d5f612b85185fad6cbc14ee5cb1c5b0
Cooper <ccubed.techno@gmail.com>
2016-06-10 23:15:07 -0400

    No more milliseconds and turn off testing

--- AngelBot.py
+++ AngelBot.py
@@ -22,7 +22,7 @@ class AngelBot(discord.Client):
         self.btoken = None
         self.creator = None
         self.references = {}
-        self.testing = True
+        self.testing = False
         self.token_bucket = {}
 
     async def setup(self):
@@ -101,7 +101,7 @@ class AngelBot(discord.Client):
                 else:
                     await self.send_message(message.channel, "Lol what?")
             elif message.content.lower().startswith("owlstats"):
-                up = timedelta(seconds=(time.time() - self.uptime))
+                up = timedelta(seconds=(time.time() - self.uptime)).split(".")[0]
                 await self.send_message(message.channel,
                                         "```AngelBot Statistics\n{} Servers\n{} Users\nUptime: {}\n{} commands total\n{:.1f} commands a second```".format(
                                             len(self.servers), sum(x.member_count for x in self.servers), str(up), self.commands, self.commands/up.total_seconds()))
