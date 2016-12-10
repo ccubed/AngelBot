959a1c827a00ff6d4ffc9f1f5438a9af99c323b4
Cooper <ccubed.techno@gmail.com>
2016-12-10 12:55:36 +0000

    Debugging notifications endpoint:

--- AList.py
+++ AList.py
@@ -324,7 +324,7 @@ class AList:
                         await self.bot.send_message(message.channel, "Something went wrong. I wasn't able to get your notifications.")
                     else:
                         jsd = json.loads(text)
-                        print(jsd)
+                        print(jsd.keys())
                         msg = "Notifications ->\n"
                         for item in jsd:
                             msg += "{0}({1}) {2}".format(item['user']['display_name'], item['user']['id'], item['value'])
