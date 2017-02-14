9af9e6ff521c4bbfb1578923328606aaeff38b54
Charles Click <CharlesClick@vertinext.com>
2017-02-14 01:39:51 -0500

    Move to owapi V3. Remove defunct hero endpoint. Fix docs.

--- Overwatch.py
+++ Overwatch.py
@@ -3,97 +3,46 @@ from discord import embeds
 
 class OWAPI:
     def __init__(self, client):
-        self.apiurl = "https://owapi.net/api/v2/u/"
-        # Map Numbers to names
-        self.index = {1: 'roadhog', 2: 'junkrat', 3: 'lucio', 4: 'soldier76', 5: 'zarya', 6: 'mccree', 7: 'tracer', 8: 'reaper',
-                      9: 'widowmaker', 10: 'winston', 11: 'pharah', 12: 'reinhardt', 13: 'symmetra', 14: 'torbjorn', 15: 'bastion',
-                      16: 'hanzo', 17: 'mercy', 18: 'zenyatta', 20: 'mei', 21: 'genji', 22: 'dva'}
-        # Map names to Numbers
-        self.characters = [{'names': ['roadhog', 'birdie'], 'id': 1}, {'names': ['junkrat', 'crocodile dun dee'], 'id': 2},
-                           {'names': ['lucio', 'lúcio', 'drop the beats'], 'id': 3}, {'names': ['soldier: 76', 'cod', 'blops'], 'id': 4},
-                           {'names': ['zarya', 'i must break you'], 'id': 5}, {'names': ['mccree', 'robo dead redemption'], 'id': 6},
-                           {'names': ['tracer', 'booty', 'bootiful'], 'id': 7}, {'names': ['reaper', 'edgelord'], 'id': 8},
-                           {'names': ['widowmaker', 'boo berry', 'boobs'], 'id': 9}, {'names': ['winston', 'dr zaius', 'caesar'], 'id': 10},
-                           {'names': ['pharah', 'gundam wing'], 'id': 11}, {'names': ['reinhardt', 'the iron giant', 'fell off the map'], 'id': 12},
-                           {'names': ['symmetra', 'chellmetra'], 'id': 13}, {'names': ['torbjorn', 'torbjörn', 'tim the turret man taylor', 'engineer'], 'id': 14},
-                           {'names': ['bastion', 'git gud'], 'id': 15}, {'names': ['hanzo', 'ryuu ga teki no'], 'id': 16},
-                           {'names': ['mercy', 'the medic', 'mother mercy'], 'id': 17}, {'names': ['zenyatta', 'android krillin'], 'id': 18},
-                           {'names': ['mei', 'bae', 'waifu'], 'id': 20}, {'names': ['genji', 'animu'], 'id': 21},
-                           {'names': ['D.Va', 'twitch streamer', 'gamer girl'], 'id': 22}]
-        self.commands = [['ow', self.ow], ['owheroes', self.owheroes], ['owhero', self.owhero]]
+        self.apiurl = "https://owapi.net/api/v3/u/"
+        self.commands = [['ow', self.ow], ['owheroes', self.owheroes]]
         self.header = {'User-Agent': "AngelBot AioHttp Python3.5"}
         self.bot = client
 
     async def ow(self, message):
         name = " ".join(message.content.split(" ")[1:]).replace('#', '-')
         with aiohttp.ClientSession() as session:
-            async with session.get(self.apiurl + "{}/stats/general".format(name), headers=self.header) as response:
+            async with session.get(self.apiurl + "{}/stats".format(name), headers=self.header) as response:
                 if response.status == 404:
                     return "Battletag not found."
                 elif response.status == 500:
                     return "Server under heavy load. Please try again later."
                 else:
                     jsd = await response.json()
-                    embed = embeds.Embed(description="Overwatch Stat Summary".format(jsd['battletag']))
-                    embed.title = jsd['battletag']
-                    if jsd['overall_stats']['avatar']:
-                        embed.set_thumbnail(url=jsd['overall_stats']['avatar'])
-                    embed.add_field(name="Rank and Level", value="**Level:** {}\n**Rank:** {}\n".format(jsd['overall_stats']['level'], jsd['overall_stats']['comprank'] or 0))
-                    embed.add_field(name="Pain Caused", value="**{}**".format(jsd['game_stats']['damage_done']))
-                    embed.add_field(name="Sad Endings", value="**{}**".format(jsd['game_stats']['deaths']))
-                    embed.add_field(name="Eliminations", value="**{}**".format(jsd['game_stats']['eliminations']))
-                    embed.add_field(name="Killshots", value="**{}**".format(jsd['game_stats']['final_blows']))
+                    region = "us" if jsd['us'] is not None else "eu" if jsd['eu'] is not None else "kr"
+                    embed = embeds.Embed(description="Overwatch Stat Summary")
+                    embed.title = name.replace("-", "#")
+                    if jsd[region]['quickplay']['overall_stats']['avatar']:
+                        embed.set_thumbnail(url=jsd[region]['quickplay']['overall_stats']['avatar'])
+                    embed.add_field(name="Rank and Level", value="**Level:** {}\n**Rank:** {}\n".format(jsd[region]['quickplay']['overall_stats']['level'], jsd[region]['quickplay']['overall_stats']['comprank'] or 0))
+                    embed.add_field(name="Pain Caused", value="**{}**".format(jsd[region]['quickplay']['overall_stats']['game_stats']['damage_done']))
+                    embed.add_field(name="Sad Endings", value="**{}**".format(jsd[region]['quickplay']['overall_stats']['game_stats']['deaths']))
+                    embed.add_field(name="Eliminations", value="**{}**".format(jsd[region]['quickplay']['overall_stats']['game_stats']['eliminations']))
+                    embed.add_field(name="Killshots", value="**{}**".format(jsd[region]['quickplay']['overall_stats']['game_stats']['final_blows']))
                     await self.bot.send_message(message.channel, embed=embed)
 
     async def owheroes(self, message):
         name = " ".join(message.content.split(" ")[1:]).replace('#', '-')
         with aiohttp.ClientSession() as session:
-            async with session.get(self.apiurl + "{}/heroes/general".format(name), headers=self.header) as response:
+            async with session.get(self.apiurl + "{}/heroes".format(name), headers=self.header) as response:
                 if response.status == 404:
                     await self.bot.send_message(message.channel, "Battletag not found.")
                 elif response.status == 500:
                     await self.bot.send_message(message.channel, "Server under heavy load. Please try again later.")
                 else:
                     jsd = await response.json()
-                    message = "{}'s Heroes.\n```xl\n".format(jsd['battletag'])
-                    for hero in [x for x in jsd['heroes'] if jsd['heroes'][x] > 0]:
-                        message += "{} - Time Played(Hrs): {}\n".format(hero, round(jsd['heroes'][hero], 2))
-                    await self.bot.send_message(message.channel, message + '```')
-
-    async def owhero(self, message):
-        name = " ".join(message.content.split(" ")[1:]).split(":")[0].replace('#', '-')
-        hname = " ".join(message.content.split(" ")[1:]).split(":")[1]
-        if hname.isdigit():
-            if hname not in self.index:
-                await self.bot.send_message(message.channel, "That's not a valid hero id.")
-            else:
-                hname = self.index[hname]
-        else:
-            found = False
-            for hero in self.characters:
-                if hname.lower() in hero['names']:
-                    hname = self.index[hero['id']]
-                    found = True
-                    break
-            if not found:
-                await self.bot.send_message(message.channel, "Couldn't find a hero with that name.")
-        with aiohttp.ClientSession() as session:
-            async with session.get(self.apiurl + "{}/heroes/{}".format(name, hname), headers=self.header) as response:
-                if response.status == 404:
-                    await self.bot.send_message(message.channel, "Couldn't find that battletag.")
-                elif response.status == 500:
-                    await self.bot.send_message(message.channel, "Server under heavy load. Please try again later.")
-                else:
-                    jsd = await response.json()
-                    messages = []
-                    message = "{}'s stats for {}\n".format(name, hname)
-                    messages.append(message)
-                    msg = "General Stats\n"
-                    for stat in [x for x in jsd['general_stats'] if 'guid' not in x]:
-                        msg += "   {}: {}\n".format(stat, jsd['general_stats'][stat])
-                    messages.append(msg)
-                    msg = "Hero Specific Stats\n"
-                    for stat in [x for x in jsd['hero_stats'] if 'guid' not in x]:
-                        msg += "   {}: {}\n".format(stat, jsd['hero_stats'][stat])
-                    messages.append(msg)
-                    await self.bot.send_message(message.channel, "\n".join(messages))
+                    region = "us" if jsd['us'] is not None else "eu" if jsd['eu'] is not None else "kr"
+                    embed = embeds.Embed(description="Hero Playtime Summary")
+                    embed.title = name.replace("-", "#")
+                    for hero in [x for x in jsd[region]['heroes']['playtime']['quickplay'] if jsd[region]['heroes']['playtime']['quickplay'][x] > 0]:
+                        embed.add_field(name=hero, value=round(jsd[region]['heroes']['playtime']['quickplay'][hero], 2))
+                    await self.bot.send_message(message.channel, embed=embed)
--- docs/source/Overwatch.rst
+++ docs/source/Overwatch.rst
@@ -2,8 +2,7 @@ Overwatch
 =========
 
 This module allows you to grab stats for battletags across the US or EU region. You can write battletags as name#ID or name-ID. The API expects name-ID but we're used to name#ID.
-
-**NOTE**: Cross region works now. I'm no longer forcing US lookups since the timeouts related to the EU lookups on Blizzard's end seem to be gone.
+* OWAPI now does multiregion automatically and AngelBot supports it.
 
 Features
 --------
@@ -13,7 +12,3 @@ ow <battletag>
 
 owheroes <battletag>
     This endpoint has changed. It now returns heroes and win rates, but no other data. This now returns any played hero on a battletag with a win rate greater than 0%.
-
-owhero <battletag>:<hero>
-    Pull stats for a specific hero for a battletag. This is where the easter eggs are. See if you can find some of my funny names for the heroes. Also accepts IDs.
-    Course you can also just go look at my source and find the names, but that's no fun.
